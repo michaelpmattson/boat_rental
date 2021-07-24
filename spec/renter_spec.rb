a388ba739c9aac05b7ebbb15f77a6661980c090b
Michael Mattson <michael.p.mattson@gmail.com>
2021-07-24 09:34:44 -0600

    Build renter

--- spec/renter_spec.rb
+++ spec/renter_spec.rb
@@ -2,13 +2,13 @@ require './lib/renter'
 
 RSpec.describe Renter do
   describe '#initialize' do
-    xit 'exists' do
+    it 'exists' do
       renter = Renter.new("Patrick Star", "4242424242424242")
 
       expect(renter).to be_an_instance_of(Renter)
     end
 
-    xit 'has attributes' do
+    it 'has attributes' do
       renter = Renter.new("Patrick Star", "4242424242424242")
 
       expect(renter.name).to eq("Patrick Star")
